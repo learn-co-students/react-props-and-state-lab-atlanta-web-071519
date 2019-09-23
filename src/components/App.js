0d54d3e34b0d15624204ba316189d510e53d0d61
tloveATL <tlove.weaver@gmail.com>
2019-09-23 17:13:27 -0400

    Done.

--- src/components/App.js
+++ src/components/App.js
@@ -15,6 +15,32 @@ class App extends React.Component {
     }
   }
 
+  onChangeType = (e) => {
+    const petType = e.target.value
+    this.setState({ filters: {type: petType}})
+  }
+
+  onFindPetsClick = (e) => {
+    const petType = this.state.filters.type
+    let url = ''
+    if(this.state.filters.type ==="all"){
+      url = "/api/pets"
+    } else{
+      url = `/api/pets?type=${petType}`
+    }
+
+    fetch(url)
+    .then(resp => resp.json())
+    .then(pets => this.setState({ pets: pets}))
+  }
+
+  onAdoptPet = (petId) => {
+    const pets = this.state.pets.map(p => {
+      return p.id === petId ? { ...p, isAdopted: true } : p;
+    });
+    this.setState({ pets });
+  };
+
   render() {
     return (
       <div className="ui container">
@@ -24,10 +50,10 @@ class App extends React.Component {
         <div className="ui container">
           <div className="ui grid">
             <div className="four wide column">
-              <Filters />
+              <Filters onChangeType={this.onChangeType} onFindPetsClick={this.onFindPetsClick}/>
             </div>
             <div className="twelve wide column">
-              <PetBrowser />
+              <PetBrowser pets={this.state.pets} onAdoptPet={this.onAdoptPet} />
             </div>
           </div>
         </div>
